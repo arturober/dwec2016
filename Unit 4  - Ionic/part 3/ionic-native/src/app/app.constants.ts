060cac404810def94f0600de2f00289f11c0b024
Arturo Bernal <arturo@iessanvicente.com>
2017-02-02 12:44:30 +0100

    constants corrected

--- Unit 4  - Ionic/part 3/ionic-native/src/app/app.constants.ts
+++ Unit 4  - Ionic/part 3/ionic-native/src/app/app.constants.ts
@@ -1,8 +1,8 @@
 // Key created for GMaps API (web version)
-export const GMAPS_API_KEY="AIzaSyB8pt95qMoUuI3GWIh_FQWd-vvoUaQKJfM";
+export const GMAPS_API_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 // ID generated when creating a new ad block in AdMob (interstitial)
-export const ADMOB_INTERSTITIAL_ID="ca-app-pub-2696023262583928/9572346891";
+export const ADMOB_INTERSTITIAL_ID="ca-app-pub-xxxxxxxxxxxxxx/yyyyyyyyyy";
 // CLIENT ID generated for Google Auth credentials (Web version)
-export const GPLUS_WEB_CLIENT_ID="857932202469-b2655f7p4gettk5ovv9e4v6qg3jiipqm.apps.googleusercontent.com";
+export const GPLUS_WEB_CLIENT_ID="xxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com";
 
 // Other keys and ID in config.xml (configure before installing plugin)
